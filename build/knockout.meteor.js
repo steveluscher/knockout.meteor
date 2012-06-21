77100669c53d56271d4deaae2b9e00b81b03964f
Ken Pratt <ken@kenpratt.net>
2012-06-21 15:00:58 -0700

    Added support for providing knockout observables as arguments to find and findOne -- query results get updated when finder params change.

--- build/knockout.meteor.js
+++ build/knockout.meteor.js
@@ -14,80 +14,194 @@ http://github.com/steveluscher/knockout.meteor
 
 
 (function() {
-  var apply_defaults, meteor, sync;
+  var AbstractFinder, FindMany, FindOne, MappedQuery, NotImplementedError, meteor,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  NotImplementedError = (function(_super) {
+
+    __extends(NotImplementedError, _super);
+
+    function NotImplementedError(func_name) {
+      this.name = 'NotImplementedError';
+      this.message = "'" + func_name + "' function must be implemented by subclass.";
+    }
+
+    return NotImplementedError;
+
+  })(Error);
 
   meteor = {
     find: function(collection, selector, options) {
-      var data_func, meteor_cursor;
       if (options == null) {
         options = {};
       }
-      apply_defaults(options);
+      return (new FindMany(collection, selector, options)).run();
+    },
+    findOne: function(collection, selector, options) {
+      if (options == null) {
+        options = {};
+      }
+      return (new FindOne(collection, selector, options)).run();
+    }
+  };
+
+  AbstractFinder = (function() {
+
+    function AbstractFinder(collection, selector, options) {
+      this.collection = collection;
+      this.selector = selector;
+      this.options = options != null ? options : {};
+      this.run = __bind(this.run, this);
+
+      this.target = null;
+      if (ko.isObservable(this.collection)) {
+        this.collection.subscribe(this.run);
+      }
+      if (ko.isObservable(this.selector)) {
+        this.selector.subscribe(this.run);
+      }
+      if (ko.isObservable(this.options)) {
+        this.options.subscribe(this.run);
+      }
+    }
+
+    AbstractFinder.prototype.run = function() {
+      var collection, options, selector;
+      if (this.query) {
+        this.query.destroy();
+      }
+      collection = ko.utils.unwrapObservable(this.collection);
+      selector = ko.utils.unwrapObservable(this.selector);
+      options = ko.utils.unwrapObservable(this.options);
+      this.applyDefaults(options);
+      this.query = this.createQuery(collection, selector, options);
+      return this.query.run();
+    };
+
+    AbstractFinder.prototype.applyDefaults = function(options) {
+      _.defaults(options, {
+        mapping: {},
+        view_model: null
+      });
+      _.defaults(options.mapping, {
+        key: function(item) {
+          return ko.utils.unwrapObservable(item._id);
+        },
+        copy: []
+      });
+      if (options.mapping.copy && _.isArray(options.mapping.copy)) {
+        options.mapping.copy = _.union(options.mapping.copy, ['_id']);
+      }
+      if (_.isFunction(options.view_model)) {
+        return options.mapping.create = function(opts) {
+          var view_model;
+          view_model = new options.view_model();
+          return ko.mapping.fromJS(opts.data, options.mapping, view_model);
+        };
+      }
+    };
+
+    AbstractFinder.prototype.createQuery = function(collection, selector, options) {
+      throw new NotImplementedError('createQuery');
+    };
+
+    return AbstractFinder;
+
+  })();
+
+  FindMany = (function(_super) {
+
+    __extends(FindMany, _super);
+
+    function FindMany() {
+      return FindMany.__super__.constructor.apply(this, arguments);
+    }
+
+    FindMany.prototype.createQuery = function(collection, selector, options) {
+      var data_func, meteor_cursor;
       meteor_cursor = collection.find(selector, options.meteor_options);
       data_func = function() {
         meteor_cursor.rewind();
         return meteor_cursor.fetch();
       };
-      return sync({}, data_func, options.mapping);
-    },
-    findOne: function(collection, selector, options) {
+      return new MappedQuery(this, data_func, options.mapping);
+    };
+
+    return FindMany;
+
+  })(AbstractFinder);
+
+  FindOne = (function(_super) {
+
+    __extends(FindOne, _super);
+
+    function FindOne() {
+      return FindOne.__super__.constructor.apply(this, arguments);
+    }
+
+    FindOne.prototype.createQuery = function(collection, selector, options) {
       var data_func;
-      if (options == null) {
-        options = {};
-      }
-      apply_defaults(options);
       data_func = function() {
         return collection.findOne(selector, options.meteor_options);
       };
-      return sync({}, data_func, options.mapping);
-    }
-  };
+      return new MappedQuery(this, data_func, options.mapping);
+    };
 
-  apply_defaults = function(options) {
-    _.defaults(options, {
-      mapping: {},
-      view_model: null
-    });
-    _.defaults(options.mapping, {
-      key: function(item) {
-        return ko.utils.unwrapObservable(item._id);
-      },
-      copy: []
-    });
-    if (options.mapping.copy && _.isArray(options.mapping.copy)) {
-      options.mapping.copy = _.union(options.mapping.copy, ['_id']);
-    }
-    if (_.isFunction(options.view_model)) {
-      return options.mapping.create = function(opts) {
-        var view_model;
-        view_model = new options.view_model();
-        return ko.mapping.fromJS(opts.data, options.mapping, view_model);
-      };
-    }
-  };
+    return FindOne;
 
-  sync = function(target, data_func, mapping) {
-    var ctx,
-      _this = this;
-    ctx = new Meteor.deps.Context();
-    ctx.on_invalidate(function() {
-      return sync(target, data_func, mapping);
-    });
-    return ctx.run(function() {
+  })(AbstractFinder);
+
+  MappedQuery = (function() {
+
+    function MappedQuery(finder, data_func, mapping) {
+      this.finder = finder;
+      this.data_func = data_func;
+      this.mapping = mapping;
+      this.execute = __bind(this.execute, this);
+
+      this.onInvalidate = __bind(this.onInvalidate, this);
+
+      this.active = true;
+    }
+
+    MappedQuery.prototype.destroy = function() {
+      return this.active = false;
+    };
+
+    MappedQuery.prototype.onInvalidate = function() {
+      if (this.active) {
+        return this.run();
+      }
+    };
+
+    MappedQuery.prototype.run = function() {
+      var ctx;
+      ctx = new Meteor.deps.Context();
+      ctx.on_invalidate(this.onInvalidate);
+      return ctx.run(this.execute);
+    };
+
+    MappedQuery.prototype.execute = function() {
       var data, result;
-      data = data_func();
-      if (target && target.__ko_mapping__) {
-        if (_.isUndefined(ko.utils.unwrapObservable(target))) {
-          return target(ko.mapping.fromJS(data, mapping));
+      data = this.data_func();
+      if (this.finder.target && this.finder.target.__ko_mapping__) {
+        if (_.isUndefined(ko.utils.unwrapObservable(this.finder.target))) {
+          this.finder.target(ko.mapping.fromJS(data, this.mapping));
         } else {
-          return ko.mapping.fromJS(data, target);
+          ko.mapping.fromJS(data, this.finder.target);
         }
       } else {
-        result = ko.mapping.fromJS(data, mapping);
-        return target = ko.isObservable(result) ? result : ko.observable(result);
+        result = ko.mapping.fromJS(data, this.mapping);
+        this.finder.target = ko.isObservable(result) ? result : ko.observable(result);
       }
-    });
-  };
+      return this.finder.target;
+    };
+
+    return MappedQuery;
+
+  })();
 
   ko.exportSymbol('meteor', meteor);
 
--- src/knockout.meteor.coffee
+++ src/knockout.meteor.coffee
@@ -10,10 +10,90 @@ ensure that the Observables are updated.
 http://github.com/steveluscher/knockout.meteor
 ###
 
+class NotImplementedError extends Error
+  constructor: (func_name) ->
+    @name = 'NotImplementedError'
+    @message = "'#{func_name}' function must be implemented by subclass."
+
+#
+# These functions are exported as ko.meteor.find and ko.meteor.findOne
+#
 meteor =
   find: (collection, selector, options = {}) ->
-    apply_defaults(options)
+    (new FindMany(collection, selector, options)).run()
 
+  findOne: (collection, selector, options = {}) ->
+    (new FindOne(collection, selector, options)).run()
+
+#
+# A Finder accepts a collection, selector, and options hash as arguments,
+# and returns a ko.observable (or ko.observableArray) that will get updated
+# whenever the data matched by the query changes, or the query parameters
+# themselves change.
+#
+# collection - a Meteor.Collection object, or
+#              a Knockout observable that returns one.
+# selector - a Mongo selector, a String, or
+#            a Knockout observable that returns a Mongo selector or a String.
+# options - an Object, or a Knockout observable that returns an Object
+#
+class AbstractFinder
+  constructor: (@collection, @selector, @options = {}) ->
+    @target = null
+
+    # If an argument to this finder happens to be a Knockout observable,
+    # subscribe to it and recreate the query whenever it changes
+    @collection.subscribe(@run) if ko.isObservable(@collection)
+    @selector.subscribe(@run) if ko.isObservable(@selector)
+    @options.subscribe(@run) if ko.isObservable(@options)
+
+  run: () =>
+    # Kill the existing query
+    @query.destroy() if @query
+
+    # Prepare the query arguments
+    collection = ko.utils.unwrapObservable(@collection)
+    selector = ko.utils.unwrapObservable(@selector)
+    options = ko.utils.unwrapObservable(@options)
+    @applyDefaults(options)
+
+    # Create a MappedQuery (as defined in subclass)
+    @query = @createQuery(collection, selector, options)
+
+    # Run the query
+    @query.run()
+
+  applyDefaults: (options) ->
+    _.defaults options,
+      mapping: {}
+      view_model: null
+
+    # Merge in some mapping defaults
+    _.defaults options.mapping,
+      # It's important to key collection members by their Mongo _id so that
+      # the Knockout Mapping plugin can determine if an object is new or old
+      key: (item) -> ko.utils.unwrapObservable(item._id),
+      copy: []
+
+    # The _id parameter is typically not something that we
+    # will change from the UI, so copy it straight into the
+    # view model without constructing it as an observable
+    if options.mapping.copy and _.isArray(options.mapping.copy)
+      options.mapping.copy = _.union(options.mapping.copy, ['_id'])
+
+    # If we were passed a view_model in the options hash,
+    # instruct the Knockout Mapping plugin to instantiate
+    # each Meteor record as an instance of that model
+    if _.isFunction options.view_model
+      options.mapping.create = (opts) ->
+        view_model = new options.view_model()
+        ko.mapping.fromJS(opts.data, options.mapping, view_model)
+
+  createQuery: (collection, selector, options) ->
+    throw new NotImplementedError('createQuery')
+
+class FindMany extends AbstractFinder
+  createQuery: (collection, selector, options) ->
     # Set up the Meteor cursor for this selector
     meteor_cursor = collection.find(selector, options.meteor_options)
 
@@ -22,62 +102,57 @@ meteor =
       meteor_cursor.rewind()
       meteor_cursor.fetch()
 
-    sync({}, data_func, options.mapping)
+    new MappedQuery(@, data_func, options.mapping)
 
-  findOne: (collection, selector, options = {}) ->
-    apply_defaults(options)
 
+class FindOne extends AbstractFinder
+  createQuery: (collection, selector, options) ->
     # This is the function we want rerun when the result of this query changes
     data_func = -> collection.findOne(selector, options.meteor_options)
 
-    sync({}, data_func, options.mapping)
+    new MappedQuery(@, data_func, options.mapping)
 
-apply_defaults = (options) ->
-  _.defaults options,
-    mapping: {}
-    view_model: null
+#
+# A MappedQuery monitors a finder for changes in its dataset. When it detects
+# that a finder's dataset has been invalidated, it reruns data_func (unless
+# query.destroy() has been called) and maps the results into finder's target.
+#
+class MappedQuery
+  constructor: (@finder, @data_func, @mapping) ->
+    @active = true
 
-  # Merge in some mapping defaults
-  _.defaults options.mapping,
-    # It's important to key collection members by their mongo _id so that the
-    # Knockout Mapping plugin can determineif an object is new or old
-    key: (item) -> ko.utils.unwrapObservable(item._id),
-    copy: []
+  destroy: () ->
+    @active = false
 
-  # The _id parameter is typically not something that we
-  # will change from the UI, so copy it straight into the
-  # view model without constructing it as an observable
-  if options.mapping.copy and _.isArray(options.mapping.copy)
-    options.mapping.copy = _.union(options.mapping.copy, ['_id'])
+  # rerun on invalidation
+  onInvalidate: () =>
+    @run() if @active
 
-  # If we were passed a view_model in the options hash,
-  # instruct the Knockout Mapping plugin to instantiate
-  # each Meteor record as an instance of that model
-  if _.isFunction options.view_model
-    options.mapping.create = (opts) ->
-      view_model = new options.view_model()
-      ko.mapping.fromJS(opts.data, options.mapping, view_model)
+  run: () ->
+    # Make use of Meteor's invalidation contexts to trigger a
+    # re-mapping of the view model when this finder's dataset changes.
+    ctx = new Meteor.deps.Context() # invalidation context
+    ctx.on_invalidate(@onInvalidate)
+    ctx.run(@execute)
 
-sync = (target, data_func, mapping) ->
-  # Make use of Meteor's invalidation contexts to trigger a re-mapping of the
-  # view model's observableArray when the Meteor collection changes
-  ctx = new Meteor.deps.Context()                        # invalidation context
-  ctx.on_invalidate(-> sync(target, data_func, mapping)) # rerun sync() on invalidation
-  ctx.run =>
+  # Should only be called from Meteor.deps.Context#run
+  execute: () =>
     # Fetch fresh data
-    data = data_func()
+    data = @data_func()
 
-    if target and target.__ko_mapping__
+    if @finder.target and @finder.target.__ko_mapping__
       # This target has already been mapped, so update it
-      if _.isUndefined(ko.utils.unwrapObservable(target))
+      if _.isUndefined(ko.utils.unwrapObservable(@finder.target))
         # There's nothing to map into, so replace the whole target
-        target(ko.mapping.fromJS(data, mapping))
+        @finder.target(ko.mapping.fromJS(data, @mapping))
       else
         # Remap to the existing target
-        ko.mapping.fromJS(data, target)
+        ko.mapping.fromJS(data, @finder.target)
     else
       # Map to this target for the first time
-      result = ko.mapping.fromJS(data, mapping)
-      target = if ko.isObservable(result) then result else ko.observable(result)
+      result = ko.mapping.fromJS(data, @mapping)
+      @finder.target = if ko.isObservable(result) then result else ko.observable(result)
+
+    return @finder.target
 
 ko.exportSymbol('meteor', meteor)
